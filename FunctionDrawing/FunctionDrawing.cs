38f21fcdfef644600d1963e003483e9104925b82
Yitang Yang <3160114036@qq.com>
2022-08-14 15:12:19 +0800

    FunctionDrawing基本功能实现

--- FunctionDrawing/FunctionDrawing.cs
+++ FunctionDrawing/FunctionDrawing.cs
@@ -3,101 +3,197 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Text;
+using static System.Math;
 
 namespace FunctionSketch
 {
     public class FunctionDrawing
     {
         List<FunctionStorage> saveFunctions = new List<FunctionStorage>();
-        DrawingGroup FuncCanvas = new DrawingGroup();
+        DrawingGroup funcsCanvas = new DrawingGroup();
+        DrawingContext brush;
         Image targetImage = null;
+        Point middlePoint = new Point(0d, 0d);
+        private readonly double dx = 0.01;
 
-        bool AutoRefresh { get; set; }
-        public FunctionDrawing(FunctionStorage[] funcsStore)
+        public double UnitNumForWidth { get; set; }
+        public double AspectRatio { get; set; }
+        public Pen FuncsPenSetting { get; set; }
+        public Pen CoordPenSetting { get; set; }
+        public bool AutoRefresh { get; set; }
+
+        public FunctionDrawing()
+        {
+            UnitNumForWidth = 20;
+            AutoRefresh = true;
+            AspectRatio = 0.618;
+            FuncsPenSetting = new Pen(Brushes.Black, 0.05);
+            CoordPenSetting = new Pen(Brushes.Gray, 0.07);
+        }
+        public FunctionDrawing(FunctionStorage[] funcsStore) : this()
         {
             AddFunction(funcsStore);
-            throw new NotImplementedException();
         }
 
         public void AddFunction(FunctionStorage[] funcsStore)
         {
-            // 此处foreach，对不同函数采取不同绘制策略（用其他多态函数）
-            throw new NotImplementedException();
+            foreach (var store in funcsStore)
+                saveFunctions.Add(store);
+            Refresh();
         }
 
+        public void AddFunction(FunctionStorage store)
+        {
+            saveFunctions.Add(store);
+            Refresh();
+        }
+
+        // 可以把体积大的函数拆开放
         public void AddFunction(Func<double, double> func)
         {
+            saveFunctions.Add(new SingleVarFuncStorage(func));
             Refresh();
-            throw new NotImplementedException();
+        }
+
+        private void DrawFunction(Func<double, double> func)
+        {
+            if (func == null)
+                return;
+
+            List<Point> points = new List<Point>();
+            (LimitRange widthLim, LimitRange heightLim) = GetDrawingArea();
+            for (double i = widthLim.from; i <= widthLim.to; i += dx)
+            {
+                double y = -func(i);
+                if (heightLim.from <= y && y <= heightLim.to)
+                    points.Add(new Point(i, y));
+            }
+
+            for (int i = 1; i < points.Count; i++)
+            {
+                brush.DrawLine(FuncsPenSetting, points[i - 1], points[i]);
+            }
+        }
+
+        private (LimitRange, LimitRange) GetDrawingArea()
+        {
+            double halfWidth = UnitNumForWidth / 2d;
+            double height = UnitNumForWidth * AspectRatio;
+            double halfHeight = height / 2d;
+            LimitRange widthLim = new LimitRange(
+                middlePoint.X - halfWidth,
+                middlePoint.X + halfWidth);
+            LimitRange heightLim = new LimitRange(
+                middlePoint.Y - halfHeight,
+                middlePoint.Y + halfHeight);
+            return (widthLim, heightLim);
         }
 
         public void AddFunction(Func<double, (double, double)> func)
         {
+            saveFunctions.Add(new ParamVarFuncStorage(func));
             Refresh();
+        }
+
+        public void DrawFunction(Func<double, (double, double)> func)
+        {
+            if (func == null)
+                return;
+
             throw new NotImplementedException();
         }
 
         public void AddFunction(Func<double, double, double> func)
         {
+            saveFunctions.Add(new DoubleVarFuncStorage(func));
             Refresh();
+        }
+
+        public void DrawFunction(Func<double, double, double> func)
+        {
+            if (func == null)
+                return;
+
             throw new NotImplementedException();
         }
 
         public void ClearAllFunction()
         {
+            saveFunctions.Clear();
             Refresh();
-            throw new NotImplementedException();
         }
 
-        public void SetOriginPosition(double xOffset, double yOffset)
+        public void SetMiddlePosition(double xOffset, double yOffset)
         {
-            Refresh();
-            throw new NotImplementedException();
+            SetMiddlePosition(new Point(xOffset, yOffset));
         }
 
-        public void SetOriginPosition(Point pointOffset)
+        public void SetMiddlePosition(Point pointOffset)
         {
+            middlePoint = pointOffset;
             Refresh();
-            throw new NotImplementedException();
         }
 
         public void SaveImageTo(Image target)
         {
             targetImage = target;
-            throw new NotImplementedException();
+            SaveImageTo();
         }
 
         public void SaveImageTo()
         {
-            // 使用上一次的target
-            throw new NotImplementedException();
+            if (targetImage != null)
+                targetImage.Source = SaveImage();
         }
 
         public DrawingImage SaveImage()
         {
-            throw new NotImplementedException();
+            return new DrawingImage(funcsCanvas);
         }
 
-        public void IncreaseUnitLength(double inc)
+        public void IncreaseUnitNumForWidth(double inc)
         {
+            UnitNumForWidth += inc;
             Refresh();
-            throw new NotImplementedException();
-        }
-
-        public void SetUnitLength(double length)
-        {
-            Refresh();
-            throw new NotImplementedException();
         }
 
         private void Refresh()
         {
-            if (AutoRefresh)
+            funcsCanvas = new DrawingGroup();
+            using (brush = funcsCanvas.Open())
             {
-                SaveImageTo();
+                DrawCoord();
+                foreach (var save in saveFunctions)
+                {
+                    DrawFunction((save as SingleVarFuncStorage)?.GetFunc());
+                    DrawFunction((save as DoubleVarFuncStorage)?.GetFunc());
+                    DrawFunction((save as ParamVarFuncStorage)?.GetFunc());
+                }
+                if (AutoRefresh)
+                    SaveImageTo();
             }
-            throw new NotImplementedException();
+        }
+
+        private void DrawCoord()
+        {
+            (LimitRange widthLim, LimitRange heightLim) = GetDrawingArea();
+
+            for (int i = (int)Ceiling(widthLim.from); i <= (int)Floor(widthLim.to); i++)
+                brush.DrawLine(CoordPenSetting, new Point(i, heightLim.from), new Point(i, heightLim.to));
+            for (int i = (int)Ceiling(heightLim.from); i <= (int)Floor(heightLim.to); i++)
+                brush.DrawLine(CoordPenSetting, new Point(widthLim.from, i), new Point(widthLim.to, i));
+        }
+    }
+
+    internal struct LimitRange
+    {
+        public double from;
+        public double to;
+
+        public LimitRange(double from, double to)
+        {
+            this.from = from;
+            this.to = to;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 using FunctionSketch;
 
 namespace 函数画板
@@ -27,28 +28,25 @@ namespace 函数画板
             test();
         }
 
+        FunctionDrawing fd = new FunctionDrawing();
+
         private void test()
         {
-            FunctionFactory ff = new FunctionFactory("0.5x,e^(0-0.5*x)*sin(20*0.5*x)");
-            var fs = (ParamVarFuncStorage)ff.GetFunctions()[0];
-            var func = fs.GetFunc();
-            List<Point> savep = new List<Point>();
-            for (double i = 0; i <= 10; i += 0.05)
-            {
-                (double, double) p = func(i);
-                savep.Add(new Point(p.Item1, p.Item2));
-            }
-            Pen pen = new Pen(Brushes.Black, 0.01);
-            DrawingGroup drawingGroup = new DrawingGroup();
-            using (DrawingContext paint = drawingGroup.Open())
-            {
-                for (int i = 1; i < savep.Count; i++)
-                {
-                    paint.DrawLine(pen, savep[i - 1], savep[i]);
-                }
-            }
-            img.Source = new DrawingImage(drawingGroup);
+            fd.AutoRefresh = false;
+            fd.AddFunction((x) => x*x);
+            fd.AddFunction(x => x);
+            fd.SaveImageTo(img);
             
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            fd.IncreaseUnitNumForWidth(0.5);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            img.Source = fd.SaveImage();
         }
     }
 }
