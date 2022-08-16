5f74ddcf6fd96bddf76fb600d61831ef9490e1f0
Yitang Yang <3160114036@qq.com>
2022-08-16 14:04:48 +0800

    利用滚动数组优化marching square

--- FunctionDrawing/DrawDoubleVarFunc.cs
+++ FunctionDrawing/DrawDoubleVarFunc.cs
@@ -19,25 +19,38 @@ namespace FunctionSketch
             int blockCntForHeight = (int)((heightLim.to - heightLim.from) / Deci + 1);
             /* 末尾加一，向上取整 */
 
-            double[,] saveValues = new double[blockCntForHeight + 1, blockCntForWidth + 1];
+            double[][] saveValues = new double[2][] // 滚动数组
+            {
+                new double[blockCntForWidth + 1],
+                new double[blockCntForWidth + 1]
+            };
+            double[] nowVals = saveValues[0], preVals = saveValues[1];
             int jcnt = 0;
             for (double j = heightLim.to; j >= heightLim.from; j -= Deci, jcnt++)
             {
                 int icnt = 0;
                 for (double i = widthLim.from; i < widthLim.to; i += Deci, icnt++)
                 {
-                    saveValues[jcnt, icnt] = func(i, j);
+                    nowVals[icnt] = func(i, j);
                     if (icnt > 0 && jcnt > 0)
                     {
                         DrawLineInSquare(new Point(i, j),
-                            saveValues[jcnt - 1, icnt - 1],
-                            saveValues[jcnt - 1, icnt],
-                            saveValues[jcnt, icnt],
-                            saveValues[jcnt, icnt - 1]);
+                            preVals[icnt - 1],
+                            preVals[icnt],
+                            nowVals[icnt],
+                            nowVals[icnt - 1]);
                         /* 请注意四个点值的顺序为从左上角开始顺时针 */
                     }
                 }
+                SwapArrays(ref nowVals, ref preVals);
             }
+        }
+
+        private void SwapArrays<T> (ref T[] arr1, ref T[] arr2)
+        {
+            T[] tmp = arr1;
+            arr1 = arr2;
+            arr2 = tmp;
         }
 
         private void DrawLineInSquare(Point point, double v1, double v2, double v3, double v4)
