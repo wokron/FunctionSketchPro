03287703f37b948bc397fd60cc63b28a491d3d76
Yitang Yang <3160114036@qq.com>
2022-08-20 20:55:52 +0800

    解决冲突

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -70,54 +70,7 @@ namespace 函数画板
                 Vector move = nowp - mouseDownp;
                 Vector coordMove = new Vector(-move.X / 100d, move.Y / 100d);
                 drawing.SetMiddlePosition(oriMiddle + coordMove);
-            }
-            
-        }
-
-        FunctionDrawing fd = new FunctionDrawing();
-
-        FunctionStorage fs;
-        private void Test()
-        {
-            FunctionFactory ff = new FunctionFactory("y=arctanx");
-            fs = ff.GetFunctions()[0];
-            fd.AddFunction(fs);
-            //fd.AddFunction(x => (Sqrt(Cos(x)) * Cos(200 * x) + Sqrt(Abs(x)) - 0.7) * Pow(4 - x * x, 0.01));
-            //fd.AddFunction(x => (2*Sin(x), 2*Cos(x)));
-            //fd.AddFunction((x, y) => y * y * y + Pow(3, x) - x * x * x - Pow(3, y));
-            fd.SaveImageTo(img);
-            
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            FunctionStorage tmp = (fs as SingleVarFuncStorage).GetDerivativeFunctionStorage();
-            fd.AddFunction(tmp);
-            fs = tmp;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            fd.ClearAllFunction();
-        }
-
-
-        bool isDown = false;
-        Point oriMiddle;
-        Point mouseDownp;
-        int detectCnt = 0;
-        private void Img_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isDown)
-            {
-                detectCnt++;
-                result.Content = $"探测次数：{detectCnt}";
-                Point nowp = e.GetPosition(img);
-                Vector move = nowp - mouseDownp;
-                Vector coordMove = new Vector(-move.X/100d, move.Y/100d);
-                fd.SetMiddlePosition(oriMiddle + coordMove);
-            }
-            
+            }    
         }
 
         private void Img_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
